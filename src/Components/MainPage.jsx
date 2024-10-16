e1885fd85fca76e56076a6d8e17c8e05e7d69397
Neel Bhattacharya <neelbhatta22@gmail.com>
2024-10-16 12:58:30 +0530

    doing :memo:

--- src/Components/MainPage.jsx
+++ src/Components/MainPage.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { RefreshCw } from "lucide-react"
+import { CopySlash, RefreshCw } from "lucide-react"
 
 const MainPage = () => {
 
@@ -46,6 +46,24 @@ const MainPage = () => {
     }
   }
 
+  const getStrengthColor = () => {
+    const strength = getPasswordStrength();
+    switch (strength) {
+      case 'Very strong':
+        return 'bg-bright-green'; // Bright green
+      case 'Strong':
+        return 'bg-yellow-500'; // Yellow
+      case 'Good':
+        return 'bg-bright-yellow'; // Bright yellow
+      case 'Weak':
+        return 'bg-red-500'; // Red
+      case 'Very weak':
+        return 'bg-bright-red'; // Bright red
+      default:
+        return 'bg-gray-200'; // Default gray color
+    }
+  };
+
   return (
     <>
       <div className='items-center'>
@@ -58,11 +76,11 @@ const MainPage = () => {
               <input
                 className=" flex-grow mr-2" value={password.length > 21 ? `${password.slice(0, 21)}...` : password}
               />
-              <div className=" mx-2 bg-green-500 text-white px-3 py-2 rounded-lg hover:bg-green-600 text-sm">
-                <span className="mr-2 text-sm font-medium text-black">{getPasswordStrength()}</span>
+              <div className={`mx-2 bg-green-500 text-white px-3 py-2 rounded-lg ${getStrengthColor()}`}>
+                <span className="text-sm  font-medium text-black">{getPasswordStrength()}</span>
               </div>
             </div>
-            <button className="bg-purple-500 text-white px-3 py-2 rounded-lg hover:bg-green-600 text-base" type="copy">
+            <button onClick={copyPassword} className="bg-purple-500 text-white px-3 py-2 rounded-lg hover:bg-red-600 text-base" >
               Copy
             </button>
           </div>
